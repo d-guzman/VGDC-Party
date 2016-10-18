74907114c3058f924389c1187bd2a8e58b7ed9cc
Kyle Wong <kylewonga@gmail.com>
2016-10-17 21:29:19 -0700

    Rudimentary board + junction spaces
    The board is fully laid out (obviously not final).  All game spaces hold
    a reference to the correct next space.  Junctions exist and can be
    manually given a secondary path option.  The path of game spaces
    correctly loops and alternate paths return to the main path correctly

---
+++ Assets/getJunction.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class getJunction : MonoBehaviour {
+
+    public GameObject spaceNextPrimary;
+    public GameObject spaceNextSecondary;
+
+    public bool setPrimarySpaceManually;
+    void Start()
+    {
+        print(transform.GetSiblingIndex());
+
+        if (!setPrimarySpaceManually)
+        {
+            int index = transform.GetSiblingIndex();
+
+
+            if (index < transform.parent.childCount)
+            {
+                GameObject nextBrotherNode = transform.parent.GetChild(index + 1).gameObject;
+                spaceNextPrimary = nextBrotherNode;
+            }
+            else
+            {
+                GameObject nextBrotherNode = transform.parent.GetChild(0).gameObject;
+                spaceNextPrimary = nextBrotherNode;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public GameObject getPrimarySpace()
+    {
+        return spaceNextPrimary;
+    }
+    public GameObject getSecondarySpace()
+    {
+        return spaceNextSecondary;
+    }
+
+}
+
--- Assets/getNextSpace.cs
+++ Assets/getNextSpace.cs
@@ -4,34 +4,35 @@ using System.Collections;
 public class getNextSpace : MonoBehaviour {
 
     // Use this for initialization
-    public GameObject nextSpace;
-
+    public GameObject spaceNext;
+    public bool setManually;
 	void Start () {
-        
-
-
-
         print(transform.GetSiblingIndex());
-        int index = transform.GetSiblingIndex();
-        
-        
-        if (index < transform.parent.childCount-1)
+        if (!setManually)
         {
-            GameObject nextBrotherNode = transform.parent.GetChild(index + 1).gameObject;
-            nextSpace = nextBrotherNode;
-        } else
-        {
-            GameObject nextBrotherNode = transform.parent.GetChild(0).gameObject;
-            nextSpace = nextBrotherNode;
-        }
-        
+            int index = transform.GetSiblingIndex();
 
 
+            if (index < transform.parent.childCount)
+            {
+                GameObject nextBrotherNode = transform.parent.GetChild(index + 1).gameObject;
+                spaceNext = nextBrotherNode;
+            }
+            else
+            {
+                GameObject nextBrotherNode = transform.parent.GetChild(0).gameObject;
+                spaceNext = nextBrotherNode;
+            }
+        }   
     }
 	
 	// Update is called once per frame
 	void Update () {
         
+    }
+    public GameObject nextSpace()
+    {
+        return spaceNext;
     }
     
 }
