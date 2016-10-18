4bb6107a153201799f39a7a79885083ec750cff7
Kyle Wong <kylewonga@gmail.com>
2016-10-17 20:50:08 -0700

    gameBoard scene created, spaces hold reference to next space in chain

---
+++ Assets/getNextSpace.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class getNextSpace : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject nextSpace;
+
+	void Start () {
+        
+
+
+
+        print(transform.GetSiblingIndex());
+        int index = transform.GetSiblingIndex();
+        
+        
+        if (index < transform.parent.childCount-1)
+        {
+            GameObject nextBrotherNode = transform.parent.GetChild(index + 1).gameObject;
+            nextSpace = nextBrotherNode;
+        } else
+        {
+            GameObject nextBrotherNode = transform.parent.GetChild(0).gameObject;
+            nextSpace = nextBrotherNode;
+        }
+        
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+    
+}
