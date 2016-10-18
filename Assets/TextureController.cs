bd2ae6262f8fcdd299ba2ae3395d7dfc0f93422f
Kyle Wong <kylewonga@gmail.com>
2016-10-17 23:08:31 -0700

    board spaces change color by tag

---
+++ Assets/TextureController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureController : MonoBehaviour {
+
+    public Material blue;
+    public Material red;
+    public Material star;
+    public Material junction;
+
+    void Start() {
+        updateTexture();
+	}
+	
+	
+    public void updateTexture()
+    {
+        if (gameObject.CompareTag("BlueSpace"))
+        {
+            GetComponentInChildren<Renderer>().material = blue;
+        }
+        else if (gameObject.CompareTag("RedSpace"))
+        {
+            GetComponentInChildren<Renderer>().material = red;
+        }
+        else if (gameObject.CompareTag("StarSpace"))
+        {
+            GetComponentInChildren<Renderer>().material = star;
+        }
+        else if (gameObject.CompareTag("JunctionSpace"))
+        {
+            GetComponentInChildren<Renderer>().material = junction;
+        }
+        else
+        {
+            print("INVALID TAG");
+        }
+    }
+}
