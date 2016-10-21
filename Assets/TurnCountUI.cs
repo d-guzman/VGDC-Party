48426f23ed7fc4750340e36d31852e47276b2397
Kyle Wong <kylewonga@gmail.com>
2016-10-20 17:17:01 -0700

    Added TurnCounter UI and pop in/out UI scripting
    Now we can have UI pop in and out of the edge of the screen at will.  Oh
    god this was painful

---
+++ Assets/TurnCountUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class TurnCountUI : MonoBehaviour {
+
+    // Use this for initialization
+    private GameObject turnCounter;
+    private Text textDisplay;
+    private Component counterScript;
+	void Start () {
+        turnCounter = GameObject.FindGameObjectWithTag("TurnCounter");
+        
+        textDisplay = gameObject.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        textDisplay.text = ""+(turnCounter.GetComponent<TurnCounter>().getTurnCount());
+
+	}
+}
---
+++ Assets/UIRevealer.cs
@@ -0,0 +1,180 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIRevealer : MonoBehaviour {
+
+    // Use this for initialization
+    public bool revealed;
+    public string anchorDirection;
+    public float speedFactor;
+    private Vector3 hiddenPosition;
+    private Vector3 revealedPosition;
+    private bool moving;
+    void Start() {
+        
+        revealedPosition = transform.localPosition;
+        /* use speedfactor of 100 for instant movement
+         * This doesn't correlate to pixels or time or anything because I'm
+         * just done with this script. 
+         * 
+         * 
+         * */
+        if (speedFactor == 0)
+        {
+            speedFactor = 1;
+        }
+
+        speedFactor = speedFactor / 100;
+        if(speedFactor > 1)
+        {
+            speedFactor = 1;
+        }
+        if (!revealed)
+        {
+            if (anchorDirection == "Up")
+            {
+
+                transform.position += Vector3.up * gameObject.GetComponent<RectTransform>().rect.height  ;
+            }
+            else if (anchorDirection == "Down")
+            {
+                transform.position += Vector3.down * gameObject.GetComponent<RectTransform>().rect.height ;
+
+            }
+            else if (anchorDirection == "Left")
+            {
+                transform.position += Vector3.left * gameObject.GetComponent<RectTransform>().rect.width  ;
+
+            }
+            else if (anchorDirection == "Right")
+            {
+                transform.position += Vector3.right * gameObject.GetComponent<RectTransform>().rect.width  ;
+
+            }
+            else
+            {
+                print("UNKNOWN WALL ANCHOR");
+            }
+
+
+        }
+
+        hiddenPosition = getHiddenPosition();
+        moving = false;
+    }
+
+    void Update() {
+       
+        if (revealed && moving)
+        {
+            moveToLocation(revealedPosition);
+        } else if (!revealed && moving)
+        {
+            moveToLocation(hiddenPosition);
+        }
+        if (Input.GetKeyDown("3"))
+        {
+            revealUI();
+        }
+        else if (Input.GetKeyDown("4"))
+        {
+            hideUI();
+        }
+    }
+    public void moveToLocation(Vector3 target)
+    {
+        transform.localPosition += (target - transform.localPosition) * (speedFactor);
+        if (Vector3.Distance(transform.localPosition, target) < 1)
+        {
+            moving = false;
+            transform.localPosition = target;
+        }
+    }
+    public void revealUI()
+    {
+        
+        revealed = true;
+        moving = true;
+        
+    }
+    public void hideUI()
+    {
+        
+        revealed = false;
+        moving = true;
+       
+    }
+
+    private Vector3 getHiddenPosition()
+    {
+        if (revealed)
+        {
+            if (anchorDirection == "Up")
+            {
+
+                return transform.localPosition + Vector3.up * gameObject.GetComponent<RectTransform>().rect.height  ;
+
+            }
+            else if (anchorDirection == "Down")
+            {
+                return transform.localPosition + Vector3.down * gameObject.GetComponent<RectTransform>().rect.height  ;
+            }
+            else if (anchorDirection == "Left")
+            {
+                return transform.localPosition + Vector3.left * gameObject.GetComponent<RectTransform>().rect.width  ;
+
+            }
+            else if (anchorDirection == "Right")
+            {
+                return transform.localPosition + Vector3.right * gameObject.GetComponent<RectTransform>().rect.width  ;
+
+            }
+            else
+            {
+                print("UNKNOWN WALL ANCHOR");
+                return transform.localPosition;
+            }
+        } else
+        {
+            return transform.localPosition;
+        }
+
+    }
+    private Vector3 getRevealedPosition()
+    {
+        if (!revealed)
+        {
+            if (anchorDirection == "Up")
+            {
+
+                return transform.localPosition - Vector3.up * gameObject.GetComponent<RectTransform>().rect.height ;
+
+            }
+            else if (anchorDirection == "Down")
+            {
+                return transform.localPosition - Vector3.down * gameObject.GetComponent<RectTransform>().rect.height  ;
+            }
+            else if (anchorDirection == "Left")
+            {
+                return transform.localPosition - Vector3.left * gameObject.GetComponent<RectTransform>().rect.width  ;
+
+            }
+            else if (anchorDirection == "Right")
+            {
+                return transform.localPosition - Vector3.right * gameObject.GetComponent<RectTransform>().rect.width  ;
+
+            }
+            else
+            {
+                print("UNKNOWN WALL ANCHOR");
+                return transform.localPosition;
+            }
+        }
+        else
+        {
+            return transform.localPosition;
+        }
+    }
+    
+}
