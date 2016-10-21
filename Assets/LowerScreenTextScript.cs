faddba34f1e22b0d29026a49e16afc1f2f78029f
Kyle Wong <kylewonga@gmail.com>
2016-10-21 16:20:51 -0700

    Added preset lines to lowerscreentext

---
+++ Assets/LowerScreenTextScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LowerScreenTextScript : MonoBehaviour {
+
+    // Use this for initialization
+    Text text;
+    public string[] textList;
+    private int textIndex;
+
+	void Start () {
+        text = GetComponent<Text>();
+        textIndex = 0;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("right"))
+        {
+            textIndex++;
+            if(textIndex >= textList.Length)
+            {
+                textIndex = 0;
+            }
+        } else if (Input.GetKeyDown("left"))
+        {
+            textIndex--;
+            if (textIndex < 0)
+            {
+                textIndex = textList.Length-1;
+            }
+        }
+        text.text = textList[textIndex];
+	}
+    public void setText(int x)
+    {
+        textIndex = x;
+    }
+    public string getText(int x)
+    {
+        return textList[x];
+    }
+}
