0ca1ddd4571d626e3244c4d748115b2651051205
d-guzman <dghost121@gmail.com>
2016-10-24 18:44:09 -0700

    Start Menu Script (Mayan)
    Mayan's start menu script.

---
+++ Assets/startMenu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+public class startMenu : MonoBehaviour {
+
+    public Button startText;
+    public Button exitText;
+     
+
+	// Use this for initialization
+	void Start () {
+        startText =startText.GetComponent<Button>();
+        exitText = exitText.GetComponent<Button>();
+	
+	}
+
+    public void ExitPress()
+    {
+        Application.Quit();
+    }
+
+    public void PartyPress()
+    {
+        SceneManager.LoadScene(1);
+    }
+         
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
