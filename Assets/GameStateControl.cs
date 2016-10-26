824415d6f1986d1a4a6d41db23a1ede40d3d39f2
Kyle Wong <kylewonga@gmail.com>
2016-10-26 14:17:48 -0700

    Created gameController to control minigame start/ending

---
+++ Assets/GameStateControl.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStateControl : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject startScreen;
+    public GameObject endScreen;
+    private bool gameOver;
+    private bool gameStart;
+	void Start () {
+        gameOver = false;
+        gameStart = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("6") && !gameStart)
+        {
+            startGame();
+        } else if (Input.GetKeyDown("6") && gameStart)
+        {
+            setGameOver(true);
+        }
+
+    }
+    public void setGameOver(bool x)
+    {
+        gameOver = x;
+        if (gameOver)
+        {
+            endScreen.GetComponent<UIRevealer>().revealUI();
+        } else
+        {
+            endScreen.GetComponent<UIRevealer>().hideUI();
+        }
+    }
+    public void startGame()
+    {
+        gameStart = true;
+        startScreen.GetComponent<UIRevealer>().hideUI();
+    }
+    public bool getGameOver()
+    {
+        return gameOver;
+    }
+}
