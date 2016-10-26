a19905e0dbab5d46b1cce67c76d6330a287b9e70
Kyle Wong <kylewonga@gmail.com>
2016-10-26 14:29:39 -0700

    rotateToDirection script

---
+++ Assets/RotateToDirection.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateToDirection : MonoBehaviour {
+
+    // Use this for initialization
+    private bool rotating;
+    private Quaternion toAngle;
+    private Quaternion fromAngle;
+    public float speed;
+    private float t;
+	void Start () {
+        fromAngle = transform.rotation;
+        toAngle = transform.rotation;
+        rotating = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (rotating)
+        {
+            if (t < 1)
+            {
+                t += Time.deltaTime * speed;
+            } else
+            {
+                rotating = false;
+                t = 1;
+            }
+            transform.rotation = Quaternion.Lerp(fromAngle, toAngle, t);
+        } 
+        
+
+    }
+    public void setAngle(Quaternion x)
+    {
+        toAngle = x;
+        t = 0;
+        rotating = true;
+    }
+
+}
