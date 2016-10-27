b42ce1064bb944c2df28a1f5003f3e60774df45d
shoshanimayan <mshoshan@uci.edu>
2016-10-26 17:36:15 -0700

    added working movement to arena
    movement speed is currently a public variable that must be set from the component section before the scene is played, I did this because i wasn't what sure what movement speed we wanted to use, but permanently setting a speed later will be easy, I suggest using a speed of 5f when testing the arena scene

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,15 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+public class PlayerController : MonoBehaviour {
+    private Rigidbody rb1;
+    public float speed;
+    void Start()
+    {
+        rb1 = GetComponent<Rigidbody>();
+        
+    }
+
+	void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVerticle = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal*speed*Time.deltaTime, 0.0f, moveVerticle*speed*Time.deltaTime);
+
+        transform.Translate(movement);
+    }
+
+    void FixedUpdate()
+    {
+        
+        
+    }
 }
