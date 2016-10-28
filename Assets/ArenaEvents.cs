32cfd0477ae8e6af89870dbb8a5fb4e09efc6a8c
shoshanimayan <mshoshan@uci.edu>
2016-10-28 03:54:16 -0700

    added deathzone and arena event script

---
+++ Assets/ArenaEvents.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArenaEvents : MonoBehaviour {
+    public GameObject player1;
+    
+	// Use this for initialization
+	void Start () {
+        
+
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject[] playerArray = { player1 };
+        //print(playerArray.Length);
+
+        
+        if (playerArray[0].activeSelf)
+        {
+            print("active");
+        }
+
+
+    }
+}
