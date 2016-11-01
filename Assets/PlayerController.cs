bfbced89fb07cc5c2b1ce6085f9e78ee3907794f
d-guzman <dghost121@gmail.com>
2016-10-31 20:11:08 -0700

    Revert "Player Rotation Mostly Correct"
    This reverts commit 9c5b974a2c8b051c454047a8c1c69d15ab491dd8.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerController : MonoBehaviour {
+    //jump
+    private bool isFalling = false;
+    private int jumpH=7;
+
+    //attack
+    private bool wasHit = false;
+    private bool punching = false;
+
+
+
+
+    //body
+    private Rigidbody rb1;
+    private float speed=5f;
+    void Start()
+    {
+        rb1 = GetComponent<Rigidbody>();
+        gameObject.SetActive(true);
+        
+    }
+
+    void punch()
+    {
+        if (Input.GetButtonDown("Fire2")&& punching==false)
+        {
+            print("punch");
+            punching = true;
+            RaycastHit enemy;
+           
+            Vector3 half = new Vector3(.2f, .2f, .2f);
+            if (Physics.BoxCast(transform.position, half, transform.forward, out enemy,Quaternion.identity, .7f))
+            {
+                print("hited");
+                enemy.collider.attachedRigidbody.AddForce(transform.forward*7,ForceMode.VelocityChange);
+
+                punching = false;
+            }
+            else
+            { punching = false; }
+        }
+    }
+
+	void Update()
+    {
+        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVerticle = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal*speed*Time.deltaTime, 0.0f, moveVerticle*speed*Time.deltaTime);
+        
+        transform.Translate(movement);
+        //print(rb1.velocity[1]);
+       // print(isFalling);
+        if (Input.GetButton("Fire1") && isFalling==false)
+        {
+            Vector3 movementjump = new Vector3(moveHorizontal * speed * Time.deltaTime, jumpH, moveVerticle * speed * Time.deltaTime);
+
+            rb1.velocity = movementjump;
+            isFalling = true;
+            //print(isFalling);
+        }
+        if (rb1.velocity[1] == 0)
+        { isFalling = false; }
+
+
+        
+    }
+  void FixedUpdate()
+    {
+        punch();
+    }
+
+    
+
+
+   
+}
--- Assets/RotateToDirection.cs
+++ Assets/RotateToDirection.cs
@@ -2,9 +2,8 @@
 using System.Collections;
 
 public class RotateToDirection : MonoBehaviour {
-    private float playerSpeed = 10.0f;
+
     // Use this for initialization
-    /*
     private bool rotating;
     private Quaternion toAngle;
     private Quaternion fromAngle;
@@ -15,17 +14,9 @@ public class RotateToDirection : MonoBehaviour {
         toAngle = transform.rotation;
         rotating = false;
 	}
-	*/
+	
 	// Update is called once per frame
 	void Update () {
-        float moveHori = Input.GetAxisRaw("Horizontal");
-        float moveVert = Input.GetAxisRaw("Vertical");
-
-        Vector3 playerMove = new Vector3(moveHori * playerSpeed * Time.deltaTime, 0.0f, moveVert * playerSpeed * Time.deltaTime);
-        Quaternion playerRotate = Quaternion.LookRotation(playerMove);
-        transform.rotation = Quaternion.Slerp(transform.rotation, playerRotate, playerSpeed * Time.deltaTime);
-
-        /*
         if (rotating)
         {
             if (t < 1)
@@ -38,15 +29,14 @@ public class RotateToDirection : MonoBehaviour {
             }
             transform.rotation = Quaternion.Lerp(fromAngle, toAngle, t);
         } 
-        */
+        
 
     }
-    /*
     public void setAngle(Quaternion x)
     {
         toAngle = x;
         t = 0;
         rotating = true;
     }
-    */
+
 }
