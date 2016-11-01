9c5b974a2c8b051c454047a8c1c69d15ab491dd8
d-guzman <dghost121@gmail.com>
2016-10-31 19:02:47 -0700

    Player Rotation Mostly Correct
    The player now properly rotates when moving, but rotates back to their
    inital position when movement stops.

---
+++ Assets/Scripts/Player/PlayerTranslation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTranslation : MonoBehaviour {
+    private float playerSpeed = 5.0f;
+	// Update is called once per frame
+	void Update () {
+        float moveHori = Input.GetAxisRaw("Horizontal");
+        float moveVert = Input.GetAxisRaw("Vertical");
+
+        Vector3 playerMove = new Vector3(moveHori * playerSpeed * Time.deltaTime, 0.0f, moveVert * playerSpeed * Time.deltaTime);
+
+        transform.Translate(playerMove);
+	}
+}
--- Assets/Scripts/Player/RotateToDirection.cs
+++ Assets/Scripts/Player/RotateToDirection.cs
@@ -2,8 +2,9 @@
 using System.Collections;
 
 public class RotateToDirection : MonoBehaviour {
-
+    private float playerSpeed = 10.0f;
     // Use this for initialization
+    /*
     private bool rotating;
     private Quaternion toAngle;
     private Quaternion fromAngle;
@@ -14,9 +15,17 @@ public class RotateToDirection : MonoBehaviour {
         toAngle = transform.rotation;
         rotating = false;
 	}
-	
+	*/
 	// Update is called once per frame
 	void Update () {
+        float moveHori = Input.GetAxisRaw("Horizontal");
+        float moveVert = Input.GetAxisRaw("Vertical");
+
+        Vector3 playerMove = new Vector3(moveHori * playerSpeed * Time.deltaTime, 0.0f, moveVert * playerSpeed * Time.deltaTime);
+        Quaternion playerRotate = Quaternion.LookRotation(playerMove);
+        transform.rotation = Quaternion.Slerp(transform.rotation, playerRotate, playerSpeed * Time.deltaTime);
+
+        /*
         if (rotating)
         {
             if (t < 1)
@@ -29,14 +38,15 @@ public class RotateToDirection : MonoBehaviour {
             }
             transform.rotation = Quaternion.Lerp(fromAngle, toAngle, t);
         } 
-        
+        */
 
     }
+    /*
     public void setAngle(Quaternion x)
     {
         toAngle = x;
         t = 0;
         rotating = true;
     }
-
+    */
 }
