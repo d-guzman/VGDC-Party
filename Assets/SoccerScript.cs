7676c029f4960f61d4fb203d3fe32fc8646b990f
TimidJay <justizl1@uci.edu>
2016-11-04 20:06:12 -0700

    Added walls and script to soccer minigame
    Adjusted ball size
    Started working on a script (attached to the ball) to make the game work
    Added walls so players won't fall into the abyss

---
+++ Assets/SoccerScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class soccerScript : MonoBehaviour {
+
+    private Player[] leftTeam, rightTeam;
+    private Vector3 leftBound, rightBound;
+    private int result = 0,leftScore=0,rightScore=0;
+    private Rigidbody rb;
+    //0 for playing, -1 for left side win, 1 for right side win
+
+    // Use this for initialization
+	void Start () {
+        leftBound = new Vector3(-21, 0, 0);
+        rightBound = new Vector3(21, 0, 0);
+        rb = GetComponent<Rigidbody>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.x < leftBound.x)
+        {
+            leftScore++;
+            this.transform.position = new Vector3(0, 50, 0);
+            rb.velocity = new Vector3(0, 0, 0);
+        }
+        if (this.transform.position.x > rightBound.x)
+        {
+            rightScore++;
+            this.transform.position = new Vector3(0, 50, 0);
+            rb.velocity = new Vector3(0, 0, 0);
+        }
+    }
+    void onTriggerEnter(Collider c)
+    {
+    }
+    public int getResult() { return result; }
+    public Player[] getWinner(int result)
+    {
+        if (result == 1) { return rightTeam; }
+        else { return leftTeam; }
+    }
+}
