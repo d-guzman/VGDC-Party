8ac37edbebccdedc1e3872a2054f958a2081744b
shoshanimayan <mshoshan@uci.edu>
2016-11-06 01:17:13 -0800

    implemented multiple controlers for jumping and hitting in arena player class

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,76 +4,169 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
     //stun
-    private bool stunned = false;
+    public bool stunned = false;
     private float timer = 5;
     
     //jump
-    private bool isFalling = false;
+    public bool isFalling = false;
     private float jumpH=7.0f;
 
     //attack
-    private bool wasHit = false;
-    private bool punching = false;
+    public bool wasHit = false;
+    public bool punching = false;
 
     //body
-    private Rigidbody rb1;
+    
     private float speed=5f;
-    private MeshRenderer player;
-//     private GameObject player;
-
+    public GameObject player;
+  
+    
     void Start()
     {
-        //player = GameObject.FindWithTag("p1"); 
-        rb1 = GetComponent<Rigidbody>();
         gameObject.SetActive(true);
         
     }
 
     void Update()
     {
-        if (stunned == false)
+        
+      
+
+            if (player.name == "P1_Model")
         {
-            //      print(player);
-            //Get input from the joystick to 
-            float moveHorizontal = Input.GetAxis("Horizontal");
-            float moveVerticle = Input.GetAxis("Vertical");
-
-            if (Input.GetButton("Fire1") && isFalling == false)
+            if (stunned == false)
             {
-                Vector3 movementjump = new Vector3(moveHorizontal * speed * Time.deltaTime, jumpH / 1.1f, moveVerticle * speed * Time.deltaTime);
 
-                rb1.velocity = movementjump;
-                isFalling = true;
+                float moveHorizontal = Input.GetAxis("P1" + "_Horizontal");
+                float moveVerticle = Input.GetAxis("P1" + "_Vertical");
+
+                if (Input.GetButton("P1" + "_Fire1") && isFalling == false)
+                {
+                    Vector3 movementjump = new Vector3(moveHorizontal * speed * Time.deltaTime, jumpH / 1.1f, moveVerticle * speed * Time.deltaTime);
+                    player.GetComponent<Rigidbody>().velocity = movementjump;
+                    isFalling = true;
+                }
+                if (player.GetComponent<Rigidbody>().velocity[1] == 0)
+                { isFalling = false; }
             }
-            if (rb1.velocity[1] == 0)
-            { isFalling = false; }
-        }
-        else
-        {
-            timer -= Time.deltaTime;
-            if (timer==0)
+            else
             {
+                player.GetComponent<Collider>().attachedRigidbody.AddForce(-player.transform.forward, ForceMode.VelocityChange);
                 stunned = false;
                 punching = false;
+
             }
-            timer = 5;
+            punch("P1");
+        }
+
+        if (player.name == "P2_Model")
+        {
+            
+            if (stunned == false)
+            {
+
+                float moveHorizontal = Input.GetAxis("P2" + "_Horizontal");
+                float moveVerticle = Input.GetAxis("P2" + "_Vertical");
+
+                if (Input.GetButton("P2" + "_Fire1") && isFalling == false)
+                {
+                    Vector3 movementjump = new Vector3(moveHorizontal * speed * Time.deltaTime, jumpH / 1.1f, moveVerticle * speed * Time.deltaTime);
+                    player.GetComponent<Rigidbody>().velocity = movementjump;
+                    isFalling = true;
+                }
+                if (player.GetComponent<Rigidbody>().velocity[1] == 0)
+                { isFalling = false; }
+            }
+            else
+            {
+                player.GetComponent<Collider>().attachedRigidbody.AddForce(-player.transform.forward, ForceMode.VelocityChange);
+                stunned = false;
+                punching = false;
+
+            }
+
+            punch("P2");
 
         }
+
+        if (player.name == "P3_Model")
+        {
+            if (stunned == false)
+            {
+
+                float moveHorizontal = Input.GetAxis("P3" + "_Horizontal");
+                float moveVerticle = Input.GetAxis("P3" + "_Vertical");
+
+                if (Input.GetButton("P3" + "_Fire1") && isFalling == false)
+                {
+                    Vector3 movementjump = new Vector3(moveHorizontal * speed * Time.deltaTime, jumpH / 1.1f, moveVerticle * speed * Time.deltaTime);
+                    player.GetComponent<Rigidbody>().velocity = movementjump;
+                    isFalling = true;
+                }
+                if (player.GetComponent<Rigidbody>().velocity[1] == 0)
+                { isFalling = false; }
+            }
+            else
+            {
+                player.GetComponent<Collider>().attachedRigidbody.AddForce(-player.transform.forward, ForceMode.VelocityChange);
+                stunned = false;
+                punching = false;
+
+            }
+
+            punch("P3");
+
+        }
+
+        if (player.name == "P4_Model")
+        {
+            
+            if (stunned == false)
+            {
+
+                float moveHorizontal = Input.GetAxis("P4" + "_Horizontal");
+                float moveVerticle = Input.GetAxis("P4" + "_Vertical");
+
+                if (Input.GetButton("P4" + "_Fire1") && isFalling == false)
+                {
+                    Vector3 movementjump = new Vector3(moveHorizontal * speed * Time.deltaTime, jumpH / 1.1f, moveVerticle * speed * Time.deltaTime);
+                    player.GetComponent<Rigidbody>().velocity = movementjump;
+                    isFalling = true;
+                }
+                if (player.GetComponent<Rigidbody>().velocity[1] == 0)
+                { isFalling = false; }
+            }
+            else
+            {
+                player.GetComponent<Collider>().attachedRigidbody.AddForce(-player.transform.forward, ForceMode.VelocityChange);
+                stunned = false;
+                punching = false;
+
+            }
+
+            punch("P4");
+
+        }
+
     }
 
-    void punch()
+
+
+   
+
+    void punch(string x)
     {
-        if (Input.GetButtonDown("Fire2")&& punching==false)
+        if (Input.GetButtonDown(x+"_Fire2")&& punching==false)
         {
             print("punch");
             punching = true;
             RaycastHit enemy;
            
-            Vector3 half = new Vector3(.5f, .5f, .5f);
-            if (Physics.BoxCast(transform.position, half, transform.forward, out enemy,Quaternion.identity, .7f))
+            Vector3 half = new Vector3(.6f, .6f, .6f);
+            if (Physics.BoxCast(player.transform.position, half, player.transform.forward, out enemy,Quaternion.identity, 1f))
             {
                 print("hited");
-                enemy.collider.attachedRigidbody.AddForce(transform.forward*7,ForceMode.VelocityChange);
+                enemy.collider.attachedRigidbody.AddForce(player.transform.forward*7,ForceMode.VelocityChange);
 
                 punching = false;
             }
@@ -90,23 +183,13 @@ public class PlayerController : MonoBehaviour {
        
     }
 
-    void jumpHit()
+    void jumpHit(string x)
     {
-        RaycastHit enemy;
-        Vector3 half = new Vector3(.5f, .5f, .5f);
-        if(Physics.BoxCast(transform.position,half,Vector3.up, out enemy, Quaternion.identity, .1f))
-        {
-            enemy.collider.attachedRigidbody.AddForce(transform.up * 7, ForceMode.VelocityChange);
-            stun();
-        }
         
     }
 
 	
-  void FixedUpdate()
-    {
-        punch();
-    }
+ 
 
     
 
