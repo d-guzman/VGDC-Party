19f02550f4b090ccf2ec694427bdd2279c253e91
TimidJay <justizl1@uci.edu>
2016-11-11 17:46:00 -0800

    Modified Soccer Script
    -Added a Player[] win to return as the winner - adjusted functions
    related to getting the winner
    -Set up a few more things in Start - makes sure the score cylinder
    visuals are set to gray
    -I need to look up or ask how to get info from the gameboard players to
    finish up team assignment in start
    -I need to look up stuff about locking controls when victory conditions
    are met
    -Need to display some game finish text when victory conditions are met

--- Assets/Scripts/SoccerScript.cs
+++ Assets/Scripts/SoccerScript.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 public class SoccerScript : MonoBehaviour {
 
-    private Player[] leftTeam, rightTeam;
+    private Player[] leftTeam, rightTeam,win;
     private GameObject scoreDisplay;
     private Vector3 leftBound, rightBound;
     private int result = 0,leftScore=0,rightScore=0,currentPoint=1;
     private Rigidbody rb;
     public Material gray,red,blue;
-    //0 for playing, -1 for left side win, 1 for right side win
+    //result: 0 for playing, -1 for left side win, 1 for right side win
 
     // Use this for initialization
 
@@ -19,45 +19,73 @@ public class SoccerScript : MonoBehaviour {
         leftBound = new Vector3(-21, 0, 0);
         rightBound = new Vector3(21, 0, 0);
         rb = GetComponent<Rigidbody>();
-    }
-
-    void Awake()
-    {
+        win = null;
         for (int i = 1; i <= 5; i++)
         {
-           // scoreDisplay[i].GetComponent<Renderer>().material = gray;
+            scoreDisplay = GameObject.Find("Point" + i.ToString());
+            scoreDisplay.GetComponent<Renderer>().material = gray;
         }
+        for(int i=0;i<4;i++)
+        {
+        }
+        leftTeam[0].transform.position = new Vector3(-5,0,-2);
+        leftTeam[1].transform.position = new Vector3(-5,0,2);
+        rightTeam[0].transform.position = new Vector3(5,0,-2);
+        rightTeam[1].transform.position = new Vector3(5,0,2);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (this.transform.position.x < leftBound.x)
+        if (this.transform.position.x < leftBound.x || this.transform.position.x > rightBound.x)
         {
-            leftScore++;
-            scoreDisplay.GetComponent<Renderer>().material = red;
-            currentPoint++;
-            scoreDisplay = GameObject.Find("Point"+currentPoint.ToString());
-            this.transform.position = new Vector3(0, 50, 0);
-            rb.velocity = new Vector3(0, 0, 0);
+            scorePoint(this.transform.position.x);
+            checkWin(leftScore, rightScore);
+            if (result!=0)
+            {
+                setWinner(result);
+            }
         }
-        if (this.transform.position.x > rightBound.x)
+    }
+
+    void scorePoint(float pos)
+    {
+        if (pos>0)
         {
             rightScore++;
             scoreDisplay.GetComponent<Renderer>().material = blue;
-            currentPoint++;
-            scoreDisplay = GameObject.Find("Point" + currentPoint.ToString());
-            this.transform.position = new Vector3(0, 50, 0);
-            rb.velocity = new Vector3(0, 0, 0);
         }
+        else if(pos<0)
+        {
+            leftScore++;
+            scoreDisplay.GetComponent<Renderer>().material = red;
+        }
+        currentPoint++;
+        scoreDisplay = GameObject.Find("Point" + currentPoint.ToString());
+        this.transform.position = new Vector3(0, 50, 0);
+        rb.velocity = new Vector3(0, 0, 0);
     }
+
     void onTriggerEnter(Collider c)
     {
     }
-    public int getResult() { return result; }
-    public Player[] getWinner(int result)
+
+    void checkWin(int l, int r) //Assumes l is leftScore, r is rightScore
     {
-        if (result == 1) { return rightTeam; }
-        else { return leftTeam; }
+        if (l == 3) { result = -1; }
+        if (r == 3) { result = 1; }
+    }
+
+    void setWinner(int res)
+    {
+        if (result == 1) { win = rightTeam; }
+        else if (result == -1) { win = leftTeam; }
+    }
+
+    public int getResult() { return result; }
+
+    public Player[] giveResults()
+    {
+        return win;
     }
 }
