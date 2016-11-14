3404125becabcf61593c2b0ec950e1f0089898fb
shoshanimayan <mshoshan@uci.edu>
2016-11-13 20:59:22 -0800

    fixed finish line, added art assests

--- Assets/Scripts/finishLineScript.cs
+++ Assets/Scripts/finishLineScript.cs
@@ -8,22 +8,27 @@ public class finishLineScript : MonoBehaviour
     public GameObject second;
     public GameObject third;
     public GameObject fourth;
-    private ArrayList placement = new ArrayList(); 
-    public GameObject events; 
+    private GameObject[] placement; 
+    public GameObject events;
+    private int n = 0;
+
+    //collider
     void OnTriggerEnter(Collider col )
     {
         //Do something like finishing the game or changing the scene
-            col.tag = "Point";
-            print("finish" + " " +col.name);
-            placement.Add(col);
-      
-
-        
+           col.tag = "Point";
+           print("finish" + " " +col.name);
+           placement[n]=col.gameObject;
+        n++;
     }
 
+    GameObject[] getList()
+    { return placement; }
+
+    //update checks to see if race is done
     void Update()
     {
-        if(placement.Count==4)
+        if(placement.Length==4)
         {
             print("game done");
             events.GetComponent<GameStateControl>().setGameOver(true);
