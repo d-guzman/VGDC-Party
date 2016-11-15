132c578097892afbbce424ffdce0fb83a98fba9a
TimidJay <justizl1@uci.edu>
2016-11-14 18:43:21 -0800

    Assigned players to teams on start
    Assignment currently on a default, commented portion would rely on
    objects from other scenes

--- Assets/Scripts/SoccerScript.cs
+++ Assets/Scripts/SoccerScript.cs
@@ -3,10 +3,11 @@ using System.Collections;
 
 public class SoccerScript : MonoBehaviour {
 
-    private Player[] leftTeam, rightTeam,win;
+    private GameObject[] leftTeam, rightTeam,win;
     private GameObject scoreDisplay;
     private Vector3 leftBound, rightBound;
     private int result = 0,leftScore=0,rightScore=0,currentPoint=1;
+    private bool started;
     private Rigidbody rb;
     public Material gray,red,blue;
     //result: 0 for playing, -1 for left side win, 1 for right side win
@@ -15,23 +16,47 @@ public class SoccerScript : MonoBehaviour {
 
     void Start()
     {
-        scoreDisplay = GameObject.Find("Point1");
         leftBound = new Vector3(-21, 0, 0);
         rightBound = new Vector3(21, 0, 0);
         rb = GetComponent<Rigidbody>();
+        leftTeam = new GameObject[2];
+        rightTeam = new GameObject[2];
+        started = false;
         win = null;
         for (int i = 1; i <= 5; i++)
         {
             scoreDisplay = GameObject.Find("Point" + i.ToString());
             scoreDisplay.GetComponent<Renderer>().material = gray;
         }
-        for(int i=0;i<4;i++)
-        {
-        }
-        leftTeam[0].transform.position = new Vector3(-5,0,-2);
-        leftTeam[1].transform.position = new Vector3(-5,0,2);
-        rightTeam[0].transform.position = new Vector3(5,0,-2);
-        rightTeam[1].transform.position = new Vector3(5,0,2);
+        scoreDisplay = GameObject.Find("Point1");
+//        GameObject[] a = GameObject.FindGameObjectsWithTag("Player");
+        int l = 0;
+        int r = 0;
+        //for (int i=0;i<4;i++)
+        //{
+        //    //Assumes 2 red, 2 blue
+        //    if (a[i].GetComponent<Player>().getSpaceType() == 0)
+        //    {
+        //        leftTeam[l] = GameObject.Find("Player"+i.ToString());
+        //        GameObject.find("P"+i.ToString()+"_Model").GetComponent<Renderer>().material=blue;
+        //        l++;
+        //    }
+        //    else
+        //    {
+        //        rightTeam[r] = GameObject.Find("Player" + i.ToString());
+        //        GameObject.find("P"+i.ToString()+"_Model").GetComponent<Renderer>().material=red;
+        //        r++;
+        //    }
+        //}
+        //temporary implementation
+        leftTeam[0] = GameObject.Find("Player1");
+        leftTeam[1] = GameObject.Find("Player2");
+        rightTeam[0] = GameObject.Find("Player3");
+        rightTeam[1] = GameObject.Find("Player4");
+        leftTeam[0].transform.position = new Vector3(-5,3,-3);
+        leftTeam[1].transform.position = new Vector3(-5,3,3);
+        rightTeam[0].transform.position = new Vector3(5,3,-3);
+        rightTeam[1].transform.position = new Vector3(5,3,3);
     }
 
     // Update is called once per frame
@@ -44,30 +69,30 @@ public class SoccerScript : MonoBehaviour {
             if (result!=0)
             {
                 setWinner(result);
+                //results board stuff, coin changes
             }
         }
     }
 
     void scorePoint(float pos)
     {
-        if (pos>0)
+        if (rightScore + leftScore < 5)
         {
-            rightScore++;
-            scoreDisplay.GetComponent<Renderer>().material = blue;
+            if (pos > 0)
+            {
+                rightScore++;
+                scoreDisplay.GetComponent<Renderer>().material = blue;
+            }
+            else if (pos < 0)
+            {
+                leftScore++;
+                scoreDisplay.GetComponent<Renderer>().material = red;
+            }
+            currentPoint++;
+            scoreDisplay = GameObject.Find("Point" + currentPoint.ToString());
         }
-        else if(pos<0)
-        {
-            leftScore++;
-            scoreDisplay.GetComponent<Renderer>().material = red;
-        }
-        currentPoint++;
-        scoreDisplay = GameObject.Find("Point" + currentPoint.ToString());
         this.transform.position = new Vector3(0, 50, 0);
         rb.velocity = new Vector3(0, 0, 0);
-    }
-
-    void onTriggerEnter(Collider c)
-    {
     }
 
     void checkWin(int l, int r) //Assumes l is leftScore, r is rightScore
@@ -84,7 +109,7 @@ public class SoccerScript : MonoBehaviour {
 
     public int getResult() { return result; }
 
-    public Player[] giveResults()
+    public Object[] giveResults()
     {
         return win;
     }
