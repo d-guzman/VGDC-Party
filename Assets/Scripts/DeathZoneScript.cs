6f4a7e03adfe5f6e55ee3067fee5e1bd4ed0e4c8
d-guzman <dghost121@gmail.com>
2016-11-14 18:57:50 -0800

    DeathZoneScript Updated
    CHANGELIST:
    - The player objects now deactivate when a player model enters them.
    - Created booleans in the DeathZoneScript that determine if a player is
    out (deactivated)
    - Created functions which return those values. (IsPlayer#Out, where # is
    1-4)
    - Created a function which SHOULD return a list of players that are
    currently active.
    
    Buglist:
    - No discernable bugs.

--- Assets/Scripts/DeathZoneScript.cs
+++ Assets/Scripts/DeathZoneScript.cs
@@ -2,11 +2,80 @@
 using System.Collections;
 
 public class DeathZoneScript : MonoBehaviour {
+    // Connect the Player objects to make it easier to deactivate them, and
+    // create corresponding boolean values to make sure that we know when they
+    // are out of play.
+    public GameObject player1;
+    private bool P1_Out = false;
 
+    public GameObject player2;
+    private bool P2_Out = false;
+
+    public GameObject player3;
+    private bool P3_Out = false;
+
+    public GameObject player4;
+    private bool P4_Out = false;
+
+    // If the the player model enters the death zone, the player GameObject will 
+    // become deactivated.
     void OnTriggerEnter(Collider other)
     {
-        print("hit");
-        other.gameObject.SetActive(false);
+        if (other.name == "P1_Model")
+        {
+            player1.SetActive(false);
+            P1_Out = true;
+            Debug.Log("Player 1 is out!");
+        }
+
+        if (other.name == "P2_Model")
+        {
+            player2.SetActive(false);
+            P2_Out = true;
+            Debug.Log("Player 2 is out!");
+        }
+
+        if (other.name == "P3_Model")
+        {
+            player3.SetActive(false);
+            P3_Out = true;
+            Debug.Log("Player 3 is out!");
+        }
+
+        if (other.name == "P4_Model")
+        {
+            player4.SetActive(false);
+            P4_Out = true;
+            Debug.Log("Player 4 is out!");
+        }
+    }
+    // --- POSSIBLY USEFUL PUBLIC FUNCTIONS
+    // GetActivePlayers - Returns a new list of the active players.
+    public GameObject[] GetActivePlayers()
+    {
+        return GameObject.FindGameObjectsWithTag("Player");
+    }
+
+    // IsPlayer#Out - Returns True if a player is deactive (therefore out),
+    //                otherwise it returns false.
+    public bool IsPlayer1Out()
+    {
+        return P1_Out;
+    }
+
+    public bool IsPlayer2Out()
+    {
+        return P2_Out;
+    }
+
+    public bool IsPlayer3Out()
+    {
+        return P3_Out;
+    }
+
+    public bool IsPlayer4Out()
+    {
+        return P4_Out;
     }
 } 
 
