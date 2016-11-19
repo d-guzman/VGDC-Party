d1f30ef956d8c0fd502f163046db5ccc7f1791f7
Kyle Wong <kylewonga@gmail.com>
2016-11-18 18:56:51 -0800

    updated mini_ui prefab

--- Assets/Scripts/TurnCounter.cs
+++ Assets/Scripts/TurnCounter.cs
@@ -6,14 +6,7 @@ public class TurnCounter : MonoBehaviour {
     public int turnCount;
     void Awake()
     {
-        if(GameObject.FindGameObjectsWithTag("TurnCounter").Length > 1)
-        {
-            DestroyObject(this.gameObject);
-        }
-        else
-        {
-            DontDestroyOnLoad(transform.gameObject);
-        }
+        
     }
     public void setTurnCount(int x)
     {
